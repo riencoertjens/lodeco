b7a1dff071c7cb8c9a9058a69d5c1cc24b8e81e6
Rien <rien.coertjens@gmail.com>
2018-10-07 20:49:16 +0200

    remove pose

--- src/webhart-base/WrapPageElement.js
+++ src/webhart-base/WrapPageElement.js
@@ -1,16 +1,7 @@
 import React from 'react'
-import posed, { PoseGroup } from 'react-pose'
 
 import Layout from 'components/Layout'
-import { DefaultPose } from './utils/poses'
 
 export const WrapPageElement = ({ element, props }) => {
-  const { key } = props.location
-  const { pose, transitionComponent } = element.props
-
-  const Pose = pose ? posed.div(pose) : DefaultPose
-
-  const UsedTransition = transitionComponent || Pose
-
   return <Layout location={props.location}>{element}</Layout>
 }

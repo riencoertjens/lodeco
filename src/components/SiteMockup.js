88297c29b9ee54f9e1be554e61e77afd126c479e
Rien <rien.coertjens@gmail.com>
2018-10-10 21:28:36 +0200

    mock-ups

--- src/components/SiteMockup.js
+++ src/components/SiteMockup.js
@@ -2,43 +2,40 @@ import React from 'react'
 import styled, { css } from 'react-emotion'
 
 const WrapperStyle = css`
-  transform: scale(0.5);
-
-  background: #333;
+  ${'' /* transform: scale(0.5); */} background: #333;
   border-radius: 40px;
   box-sizing: content-box;
-  margin: 50px auto;
   position: relative;
-  &:after,
-  &:before {
-    content: '';
-    display: inline-block;
-    background: black;
-    position: absolute;
-  }
-  &:after {
-    width: 40px;
-    border-radius: 50%;
-    height: 40px;
-    bottom: 10px;
-    margin-left: 50%;
-    left: -22px;
-    border: 2px solid #555;
-  }
-  &:before {
-    width: 60px;
-    border-radius: 1000px;
-    height: 8px;
-    top: 26px;
-    margin-left: 50%;
-    left: -30px;
-  }
+  display: flex;
+  align-items: stretch;
+  box-shadow: 3px 3px 0 #bbb, 6px 6px 0 #bbb, 9px 9px 0 #bbb,
+    14px 10px 20px #666;
+  overflow: hidden;
   iframe,
   img {
     border: none;
     margin: 0;
     width: 100%;
-    height: 100%;
+    min-height: 100%;
+    display: block;
+  }
+  &:before {
+    content: '';
+    float: left;
+  }
+  &:after {
+    width: 36px;
+    height: 36px;
+    margin-left: -20px;
+    bottom: 10px;
+    left: 50%;
+    content: '';
+    border-radius: 50%;
+    background: black;
+    border: 2px solid #aaa;
+    position: absolute;
+    display: block;
+    z-index: 999999;
   }
 `
 
@@ -49,41 +46,33 @@ const WebsiteWrapper = styled.div`
     props.phone &&
     `
     padding: 60px 10px;
-    width: 320px;
-    height: 568px;
+    width: 300px;
+    &:before{padding-top: ${(100 * 16) / 9}%;}
   `};
   ${props =>
     props.tablet &&
     `
     padding: 60px 10px;
-    width: 1024px;
-    height: 1366px;
+    width: 600px;
+    &:before{padding-top: ${(100 * 4) / 3}%;}
+
   `};
   ${props =>
     props.desktop &&
     `
-    padding: 50px 50px 200px;
-    width: 1921px;
-    height: 1080px;
+    padding: 0 0 200px;
+    width: 2000px;
+    &:before{padding-top: ${(100 * 9) / 16}%;}
     background: #CCC;
+    iframe, img{
+      border: 60px solid black;
+    }
     &:after {
       width: 100px;
       height: 100px;
-      border-radius: 50%;
-      bottom: 50px;
-      margin-left: 50%;
+      margin-left: -50px;
+      bottom: 40px;
       border: none;
-      left: -50px;
-    }
-    &:before {
-      width: 4px;
-      height: 4px;
-      border-radius: 1000px;
-      border: 6px solid #333;
-      background: #000;
-      top: 22px;
-      margin-left: 50%;
-      left: -8px;
     }
   `};
 `
--- src/pages/work.js
+++ src/pages/work.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from 'components/Hero'
 import { StaticQuery, graphql } from 'gatsby'
-import styled from 'react-emotion'
+import styled, { css } from 'react-emotion'
 import { Phone, Tablet, Desktop } from 'components/SiteMockup'
 
 export default () => (
@@ -39,9 +39,39 @@ export default () => (
           <h1>My work</h1>
         </Hero>
         <section>
-          <Phone site="https://www.lebuindhaese.be/" />
-          <Tablet site="https://www.lebuindhaese.be/" />
-          <Desktop site="https://www.lebuindhaese.be/" />
+          <div
+            className={css`
+              transform: scale(0.5);
+              perspective: 2500px;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              & > * {
+                flex-grow: 0;
+                flex-shrink: 0;
+                margin: 0 30px;
+              }
+            `}
+          >
+            <Tablet
+              site="https://www.lebuindhaese.be/"
+              className={css`
+                transform: rotateX(0deg) rotateY(45deg) rotateZ(0deg);
+              `}
+            />
+            <Desktop
+              site="https://www.lebuindhaese.be/"
+              className={css`
+                /* transform: rotateX(0deg) rotateY(0) rotateZ(0deg); */
+              `}
+            />
+            <Phone
+              site="https://www.lebuindhaese.be/"
+              className={css`
+                transform: rotateX(0deg) rotateY(-45deg) rotateZ(0deg);
+              `}
+            />
+          </div>
         </section>
       </React.Fragment>
     )}

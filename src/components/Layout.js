8c5d09eb4267b86d8e5a5d76fc006cab939234fd
Rien Coertjens <rien.coertjens@gmail.com>
2022-11-05 15:20:31 +0100

    remove injectglobal

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-import { injectGlobal } from '@emotion/css'
+import { css, Global } from '@emotion/react'
 import styled from '@emotion/styled'
 import React from 'react'
 import Helmet from 'react-helmet'
@@ -11,40 +11,44 @@ import 'typeface-roboto-slab'
 import { colors } from '../webhart-base/utils/style'
 import Nav from './Navigation'
 
-// Global style setup
-injectGlobal`
-  
-  *::selection{
-    color: white;
-    background: ${colors.primaryBG};
-    text-shadow: 1px 1px 0 black;
-  }
-  h1, h2{
-    position: relative;
-    &:after{
-      content: '';
-      width: 200px;
-      height: 1px;
-      background-image: linear-gradient(to right, black 33%, rgba(255,255,255,0) 0%);
-      background-position: bottom;
-      background-size: 7px 1px;
-      background-repeat: repeat-x;
-      position: absolute;
-      bottom: -0.75rem;
-      right: 50%;
-      margin-right: -100px;
-    }
-  }
-  a{
-    text-decoration-skip: objects;
-    color: inherit;
-  }
-`
-
 const AppWrapper = styled('main')``
 
 const Layout = ({ element, props }) => (
   <React.Fragment>
+    <Global
+      styles={css`
+        *::selection {
+          color: white;
+          background: ${colors.primaryBG};
+          text-shadow: 1px 1px 0 black;
+        }
+        h1,
+        h2 {
+          position: relative;
+          &:after {
+            content: '';
+            width: 200px;
+            height: 1px;
+            background-image: linear-gradient(
+              to right,
+              black 33%,
+              rgba(255, 255, 255, 0) 0%
+            );
+            background-position: bottom;
+            background-size: 7px 1px;
+            background-repeat: repeat-x;
+            position: absolute;
+            bottom: -0.75rem;
+            right: 50%;
+            margin-right: -100px;
+          }
+        }
+        a {
+          text-decoration-skip: objects;
+          color: inherit;
+        }
+      `}
+    />
     <Helmet key="app-head">
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Helmet>
